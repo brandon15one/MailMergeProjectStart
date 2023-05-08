a3f8d7a19928de83f16f8a323067890234b6eddd
Siddhant Baghel <87269873+brandon15one@users.noreply.github.com>
2023-05-08 20:02:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-def listToString(s):
-    str1 = " "
-
-    return str1.join(s)
-
-
 with open("Input/Letters/starting_letter.txt", mode="r") as l:
-    letter = l.readlines()
-    join = listToString(letter)
+    letter = l.read()
     with open("Input/Names/invited_names.txt", mode="r") as n:
         names = n.readlines()
         for name in names:
-            change = join.replace("[name]", name.strip())
+            change = letter.replace("[name]", name.strip())
             with open(f"Output/ReadyToSend/{name.strip()}.txt", mode="w") as k:
                 k.write(change)
 
